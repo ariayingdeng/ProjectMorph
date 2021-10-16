734bb549321704f39977249606e1292e0d1098d3
Ying Deng <dengy9@student.douglascollege.ca>
2021-10-16 14:59:06 -0700

    Created a User class with fields user name, password, birthdate, email and id.

---
+++ src/main/java/com/csis3275/morphModel/User.java
@@ -0,0 +1,42 @@
+package com.csis3275.morphModel;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String birthdate;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
