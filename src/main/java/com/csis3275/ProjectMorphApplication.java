06998de41a8111ad0f923fb6a26001a9731f02a4
Ying Deng <dengy9@student.douglascollege.ca>
2021-10-16 11:08:48 -0700

    Added application properties

---
+++ src/main/java/com/csis3275/ProjectMorphApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMorphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMorphApplication.class, args);
+	}
+
+}
