5ed4c27a770368a9a9bca4eb2fb5f1fe545bd79c
Ying Deng <dengy9@student.douglascollege.ca>
2021-10-17 21:33:00 -0700

    Modified model class User to make it an entity

--- src/main/java/com/csis3275/morphModel/User.java
+++ src/main/java/com/csis3275/morphModel/User.java
@@ -1,12 +1,41 @@
 package com.csis3275.morphModel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
+	@Id
+	@GeneratedValue
 	private int id;
+	
+	@Column(name="username", nullable=false, unique=true)
 	private String username;
+	
+	@Column(name="password", nullable=false)
 	private String password;
+	
+	@Column(name="birthdate", nullable=true)
 	private String birthdate;
+	
+	@Column	(name="email", nullable=false)
 	private String email;
 	
+	public User() {
+		
+	}
+	
+	public User(String username, String password, String birthdate, String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.birthdate = birthdate;
+		this.email = email;
+	}
 	public int getId() {
 		return id;
 	}
