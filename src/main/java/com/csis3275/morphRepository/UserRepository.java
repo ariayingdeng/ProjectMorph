1518c38cef7350373176e34960e56c385500572d
Jin Chin <chinj7@student.douglascollege.ca>
2021-10-18 23:29:50 -0700

    Added new moprhRepository class, Added new Images for Version 2

---
+++ src/main/java/com/csis3275/morphRepository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.csis3275.morphRepository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.csis3275.morphModel.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	User findByUsername(String username);
+}
