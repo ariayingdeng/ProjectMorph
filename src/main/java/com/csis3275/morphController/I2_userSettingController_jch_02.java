ebed99db693cf447a9cff8175198bb3100b3d3b5
Jin Chin <chinj7@student.douglascollege.ca>
2021-11-06 15:16:30 -0700

    Added new userSettingController and userSetting jsp for the update basic information feature

---
+++ src/main/java/com/csis3275/morphController/I2_userSettingController_jch_02.java
@@ -0,0 +1,54 @@
+package com.csis3275.morphController;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.csis3275.morphModel.User;
+import com.csis3275.morphRepository.UserRepository;
+
+@Controller
+public class I2_userSettingController_jch_02 {
+	
+	@Autowired
+	UserRepository userRepo;
+
+	@RequestMapping("/userSetting")
+	@PostMapping("/userSetting")
+	public String userSetting(@ModelAttribute("setting")User user, Model model, HttpSession session) {
+		if (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn")) {
+			User loggedUser = userRepo.findById((int) session.getAttribute("userId"));
+			model.addAttribute("loggedIn", loggedUser.getUsername());
+			int id = (int) session.getAttribute("userId");
+			User thisUser = (User) userRepo.findById(id);
+			model.addAttribute("setting",user);
+			model.addAttribute("userName", thisUser.getUsername());
+			return "userSetting";
+		}
+		return "userLogin";
+	}
+	
+	@PostMapping("/userSettingInput")
+	public String userSettingUpdate(@ModelAttribute("setting")User user, Model model, HttpSession session) {
+		int id = (int) session.getAttribute("userId");
+		User thisUser = (User) userRepo.findById(id);			
+		if(!user.getUsername().equals("")){
+			thisUser.setUsername(user.getUsername());
+		}
+
+		if(!user.getPassword().equals("")){
+			thisUser.setPassword(user.getPassword());
+		}
+		
+		if(!user.getEmail().equals("")){
+			thisUser.setEmail(user.getEmail());
+		}
+		
+		return "morphHome";
+	}
+}
