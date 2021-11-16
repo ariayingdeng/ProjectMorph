46145fc611e7b9929e07c63bd9ddd5584b062fe6
Ying Deng <dengy9@student.douglascollege.ca>
2021-11-16 13:21:59 -0800

    Added password encryption when user updates password

---
+++ src/main/java/com/csis3275/morphController/I2_userSettingController_jch_02.java
@@ -0,0 +1,70 @@
+package com.csis3275.morphController;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.csis3275.morphModel.User;
+import com.csis3275.morphRepository.UserRepository;
+
+@Controller
+public class I2_userSettingController_jch_02 {
+	
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	UserRepository userRepo;
+
+	@RequestMapping("/userSetting")
+	@PostMapping("/userSetting")
+	public String userSetting(@ModelAttribute("setting")User user, Model model, HttpSession session) {
+		if (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn")) {
+			User loggedUser = userRepo.findById((int) session.getAttribute("userId"));
+			model.addAttribute("loggedIn", loggedUser.getUsername());
+			int id = (int) session.getAttribute("userId");
+			User thisUser = (User) userRepo.findById(id);
+			model.addAttribute("setting",user);
+			model.addAttribute("userName", thisUser.getUsername());
+			return "/userSetting";
+		}
+		return "/userLogin";
+	}
+	
+	@PostMapping("/userSettingInput")
+	public String userSettingUpdate(@ModelAttribute("setting")User user, Model model, HttpSession session) {
+		bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
+		int id = (int) session.getAttribute("userId");
+		User thisUser = (User) userRepo.findById(id);
+		model.addAttribute("userName",thisUser.getUsername());
+		
+		boolean update = false;
+		if(!user.getUsername().equals("")){
+			thisUser.setUsername(user.getUsername());
+			update=true;
+		}
+
+		if(!user.getPassword().equals("")){
+			String encrptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+			thisUser.setPassword(encrptedPassword);
+			update=true;
+		}
+		
+		if(!user.getEmail().equals("")){
+			thisUser.setEmail(user.getEmail());
+			update=true;
+		}
+
+		if(update) {
+			String success = "Hi " + thisUser.getUsername() + " , You have successfully updated your Information !";
+			model.addAttribute("updateSuccess", success);
+		}
+		
+		return "/userSetting";
+	}
+}
